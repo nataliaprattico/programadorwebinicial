f349660ace8664155322cea32549845adeedd719
Natalia Prattico <natalia.prattico@gmail.com>
2025-05-25 00:27:02 -0300

    m2u1

---
+++ m2u1/ejercicio1.js
@@ -0,0 +1,11 @@
+document.getElementById("rojo").addEventListener("click", function() {
+  document.body.style.backgroundColor = "red";
+});
+
+document.getElementById("verde").addEventListener("click", function() {
+  document.body.style.backgroundColor = "green";
+});
+
+document.getElementById("azul").addEventListener("click", function() {
+  document.body.style.backgroundColor = "blue";
+});
