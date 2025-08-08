cb3299ffa89f19d222cc54eac5de4d43f579c56e
Natalia Prattico <natalia.prattico@gmail.com>
2025-08-07 23:39:03 -0300

    crud parte 1

--- blum_node/routes/admin/novedades.js
+++ blum_node/routes/admin/novedades.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
 
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
   res.render('admin/novedades',{
     layout: 'admin/layout',
     usuario: req.session.nombre,
+    novedades
   });
+});
+
+//para eliminar una novedad//
+router.get('/eliminar/:id', async (req, res, next) => {
+  const id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
 });
 
 module.exports = router;
