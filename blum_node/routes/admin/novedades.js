8bb96ae825a79c273a074a53ef204135f4ba374f
Natalia Prattico <natalia.prattico@gmail.com>
2025-08-09 19:00:36 -0300

    CRUD 2

--- blum_node/routes/admin/novedades.js
+++ blum_node/routes/admin/novedades.js
@@ -4,7 +4,7 @@ var novedadesModel = require('./../../models/novedadesModel');
 
 router.get('/', async function (req, res, next) {
   var novedades = await novedadesModel.getNovedades();
-  res.render('admin/novedades',{
+  res.render('admin/novedades', {
     layout: 'admin/layout',
     usuario: req.session.nombre,
     novedades
@@ -17,5 +17,66 @@ router.get('/eliminar/:id', async (req, res, next) => {
   await novedadesModel.deleteNovedadesById(id);
   res.redirect('/admin/novedades')
 });
+
+//formulario para poder completar campos para agregar una novedad//
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  });
+});
+
+//enviar los datos "de la nueva novedad"//
+router.post('/agregar', async (req, res, next) => {
+  try {
+    console.log(req.body)
+    if (req.body.titulo != "" && req.body.subtitulo != "" &&
+      req.body.cuerpo != "") {
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect('/admin/novedades')
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se cargó la novedad'
+    });
+  }
+});
+
+//llamar al diseño modificar.hbs + que cargue la novedad por el id//
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  var novedad = await novedadesModel.getNovedadById(id);
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  });
+});
+
+//modificar los datos//
+router.post('/modificar', async (req, res, next) => {
+  console.log(req.body)
+  try {
+    let obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+  }
+  catch (error) {
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se modifico la novedad'
+    });
+  }
+}); 
 
 module.exports = router;
